73f8d2f1203c1de6be1da9ab996727d02b3e2393
Stuart Hostler <Stuart.Hostler@infusionsoft.com>
2015-08-13 14:59:57 -0700

    First Commit

---
+++ src/Streams_01/Streams_01.java
@@ -0,0 +1,11 @@
+package Streams_01;
+
+/**
+ * @author stuart-hostler
+ * @since 8/12/15.
+ */
+public class Streams_01 {
+    public static void main(String[] args) {
+        System.out.println("hello world 01");
+    }
+}
---
+++ src/Streams_02/Streams_02.java
@@ -0,0 +1,11 @@
+package Streams_02;
+
+/**
+ * @author stuart-hostler
+ * @since 8/12/15.
+ */
+public class Streams_02 {
+    public static void main(String[] args) {
+        System.out.println("hello world 02");
+    }
+}
